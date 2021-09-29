9dee0bc971d18a8a6b70342c0c6aa42028b0ba8b
gueckmooh <gueckmooh@pm.me>
2021-09-29 17:37:13 +0200

    Use video mode

---
+++ src/window/include/window/video_mode.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <cstdint>
+
+namespace window {
+
+class VideoMode {
+private:
+    uint32_t fWidth;
+    uint32_t fHeight;
+    uint32_t fBitsPerPixel;
+public:
+    VideoMode(uint32_t width = 0u, uint32_t height = 0u, uint32_t bitsPerPixel = 32u)
+        : fWidth(width)
+        , fHeight(height)
+        , fBitsPerPixel(bitsPerPixel)
+        {}
+
+    /// Getters
+    const uint32_t width() const { return fWidth; }
+    uint32_t& width() { return fWidth; }
+    const uint32_t height() const { return fHeight; }
+    uint32_t& height() { return fHeight; }
+    const uint32_t bitsPerPixel() const { return fBitsPerPixel; }
+    uint32_t& bitsPerPixel() { return fBitsPerPixel; }
+
+    const uint32_t bytesPerPixel() const { return fBitsPerPixel / 8; }
+    void setBytesPerPixel(const uint32_t bps) { fBitsPerPixel = bps * 8; }
+};
+
+}
